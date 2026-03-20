e4eabe3f4ca59691a7a7d72104572a9b446d8ea1
binsg0d <bartosz21082004@gmail.com>
2026-03-20 10:30:11 +0100

    Zadanie

---
+++ components/Header.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+
+
+type HeaderProps = {
+  title: string;
+};
+export default function Header({ title }: HeaderProps) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    backgroundColor: "#4A90E2",
+  },
+  title: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+});
